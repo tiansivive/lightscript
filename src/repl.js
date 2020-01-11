943d5b261fa3ab3aa77c82f50817190361b4c70c
tiansivive <tiago.vilaverde@neo4j.com>
2020-01-11 18:36:12 +0200

    adding a REPL

---
+++ src/repl.js
@@ -0,0 +1,34 @@
+import readline from 'readline'
+
+import { Parser, Grammar } from 'nearley'
+import { evaluate } from './evaluation'
+
+import lightscript from '../grammar/lightscript'
+
+
+const grammar = Grammar.fromCompiled(lightscript)
+let scope = { identifiers: [] }
+let parser = new Parser(grammar)
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+
+rl.on('line', line => {
+    
+  parser.feed(line)
+
+  const ast = parser.results[0]
+  const ambiguity = parser.results.length
+
+  const res = evaluate(ast, scope)
+  console.log('Evaluated:')
+  console.log('scope:', res.scope)
+  console.log('results:', res.value)
+  scope = res.scope
+  parser = new Parser(grammar)
+})
